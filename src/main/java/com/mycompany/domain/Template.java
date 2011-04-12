ce9a1077a8b1f7129801648434d3e91c2aa4b602
roofimon <roofimon@gmail.com>
2011-04-12 07:36:01 +0700

    First Commit

---
+++ src/main/java/com/mycompany/domain/Template.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.domain;
+
+/**
+ *
+ * @author roofimon
+ */
+class Template {
+    private String template;
+    public Template(String template){
+        this.template = template;
+    }
+
+    public void set(String val){
+
+    }
+
+    public String evaluate(){
+        return null;
+    }
+}
